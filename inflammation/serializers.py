d9e493a650708a2beefb795e42fea43e905aab8a
annannfryingpan <ann.gledson@manchester.ac.uk>
2022-05-25 13:44:02 +0100

    bits and peices

---
+++ inflammation/serializers.py
@@ -0,0 +1,23 @@
+# file: inflammation/serializers.py
+
+from inflammation import models
+
+
+class PatientSerializer:
+    model = models.Patient
+
+    @classmethod
+    def serialize(cls, instances):
+        raise NotImplementedError
+
+    @classmethod
+    def save(cls, instances, path):
+        raise NotImplementedError
+
+    @classmethod
+    def deserialize(cls, data):
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, path):
+        raise NotImplementedError
